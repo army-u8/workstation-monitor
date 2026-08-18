fbf2b2352c3c54c1b9649de222d86d6f96c17fd5
mutallip <mutalifu@weimill.com>
2026-08-17 22:59:40 -0700

    feat(icons): migrate from solid-icons/tb to official @tabler/icons-solidjs

--- frontend/src/components/Icons.tsx
+++ frontend/src/components/Icons.tsx
@@ -1,119 +1,119 @@
 import {
-  TbOutlineActivity,
-  TbOutlineAlertTriangle,
-  TbOutlineAntenna,
-  TbOutlineBolt,
-  TbOutlineBook,
-  TbOutlineBox,
-  TbOutlineBrain,
-  TbOutlineBrandChrome,
-  TbOutlineBrandGithub,
-  TbOutlineBroadcast,
-  TbOutlineCheck,
-  TbOutlineClock,
-  TbOutlineCode,
-  TbOutlineCompass,
-  TbOutlineCopy,
-  TbOutlineCpu,
-  TbOutlineDashboard,
-  TbOutlineDatabase,
-  TbOutlineDeviceDesktop,
-  TbOutlineDisc,
-  TbOutlineExternalLink,
-  TbOutlineEye,
-  TbOutlineEyeOff,
-  TbOutlineFileText,
-  TbOutlineFlame,
-  TbOutlineFolder,
-  TbOutlineGauge,
-  TbOutlineGitBranch,
-  TbOutlineHistory,
-  TbOutlineKey,
-  TbOutlineLayersIntersect,
-  TbOutlineLayoutGrid,
-  TbOutlineList,
-  TbOutlineMenu2,
-  TbOutlineMoon,
-  TbOutlinePackage,
-  TbOutlinePlug,
-  TbOutlineRefresh,
-  TbOutlineRobot,
-  TbOutlineRocket,
-  TbOutlineSearch,
-  TbOutlineServer,
-  TbOutlineShield,
-  TbOutlineSparkles,
-  TbOutlineSun,
-  TbOutlineTerminal2,
-  TbOutlineTrash,
-  TbOutlineUser,
-  TbOutlineWorld,
-} from 'solid-icons/tb';
+  IconActivity,
+  IconAlertTriangle,
+  IconAntenna,
+  IconBolt,
+  IconBook,
+  IconBox,
+  IconBrain,
+  IconBrandChrome,
+  IconBrandGithub,
+  IconBroadcast,
+  IconCheck,
+  IconClock,
+  IconCode,
+  IconCompass,
+  IconCopy,
+  IconCpu,
+  IconDashboard,
+  IconDatabase,
+  IconDeviceDesktop,
+  IconDisc,
+  IconExternalLink,
+  IconEye,
+  IconEyeOff,
+  IconFileText,
+  IconFlame,
+  IconFolder,
+  IconGauge,
+  IconGitBranch,
+  IconHistory,
+  IconKey,
+  IconLayersIntersect,
+  IconLayoutGrid,
+  IconList,
+  IconMenu2,
+  IconMoon,
+  IconPackage,
+  IconPlug,
+  IconRefresh,
+  IconRobot,
+  IconRocket,
+  IconSearch,
+  IconServer,
+  IconShield,
+  IconSparkles,
+  IconSun,
+  IconTerminal2,
+  IconTrash,
+  IconUser,
+  IconWorld,
+} from '@tabler/icons-solidjs';
 
 // Navigation & Section Tabler Icons
-export const OverviewIcon = TbOutlineDashboard;
-export const ObsidianIcon = TbOutlineBook;
-export const NoteIcon = TbOutlineFileText;
-export const ActivityIcon = TbOutlineActivity;
-export const SocketsIcon = TbOutlineBroadcast;
-export const LatencyIcon = TbOutlineBolt;
-export const SnifferIcon = TbOutlineLayersIntersect;
-export const ProcessIcon = TbOutlineCpu;
-export const DiskIcon = TbOutlineDatabase;
-export const DevToolsIcon = TbOutlineTerminal2;
-export const OpsIcon = TbOutlineShield;
-export const SearchIcon = TbOutlineSearch;
-export const RefreshIcon = TbOutlineRefresh;
-export const TrashIcon = TbOutlineTrash;
-export const SunIcon = TbOutlineSun;
-export const MoonIcon = TbOutlineMoon;
-export const SystemThemeIcon = TbOutlineDeviceDesktop;
+export const OverviewIcon = IconDashboard;
+export const ObsidianIcon = IconBook;
+export const NoteIcon = IconFileText;
+export const ActivityIcon = IconActivity;
+export const SocketsIcon = IconBroadcast;
+export const LatencyIcon = IconBolt;
+export const SnifferIcon = IconLayersIntersect;
+export const ProcessIcon = IconCpu;
+export const DiskIcon = IconDatabase;
+export const DevToolsIcon = IconTerminal2;
+export const OpsIcon = IconShield;
+export const SearchIcon = IconSearch;
+export const RefreshIcon = IconRefresh;
+export const TrashIcon = IconTrash;
+export const SunIcon = IconSun;
+export const MoonIcon = IconMoon;
+export const SystemThemeIcon = IconDeviceDesktop;
 
-export const CleanerIcon = TbOutlineTrash;
-export const GitIcon = TbOutlineGitBranch;
-export const GithubIcon = TbOutlineBrandGithub;
-export const UserIcon = TbOutlineUser;
-export const HostsIcon = TbOutlineWorld;
-export const SpeedIcon = TbOutlineGauge;
-export const FolderIcon = TbOutlineFolder;
-export const CodeIcon = TbOutlineCode;
-export const TerminalIcon = TbOutlineTerminal2;
-export const ExternalLinkIcon = TbOutlineExternalLink;
+export const CleanerIcon = IconTrash;
+export const GitIcon = IconGitBranch;
+export const GithubIcon = IconBrandGithub;
+export const UserIcon = IconUser;
+export const HostsIcon = IconWorld;
+export const SpeedIcon = IconGauge;
+export const FolderIcon = IconFolder;
+export const CodeIcon = IconCode;
+export const TerminalIcon = IconTerminal2;
+export const ExternalLinkIcon = IconExternalLink;
 
-export const GridIcon = TbOutlineLayoutGrid;
-export const ListIcon = TbOutlineList;
-export const CompactIcon = TbOutlineMenu2;
-export const ClockIcon = TbOutlineClock;
+export const GridIcon = IconLayoutGrid;
+export const ListIcon = IconList;
+export const CompactIcon = IconMenu2;
+export const ClockIcon = IconClock;
 
-export const MonitorIcon = TbOutlineDeviceDesktop;
-export const SafariIcon = TbOutlineCompass;
-export const ChromeIcon = TbOutlineBrandChrome;
-export const AppBoxIcon = TbOutlineBox;
-export const ServerIcon = TbOutlineServer;
-export const DiscIcon = TbOutlineDisc;
+export const MonitorIcon = IconDeviceDesktop;
+export const SafariIcon = IconCompass;
+export const ChromeIcon = IconBrandChrome;
+export const AppBoxIcon = IconBox;
+export const ServerIcon = IconServer;
+export const DiscIcon = IconDisc;
 
 // Standard Tabler Vector Icons across all views
-export const BoltIcon = TbOutlineBolt;
-export const RocketIcon = TbOutlineRocket;
-export const PlugIcon = TbOutlinePlug;
-export const GlobeIcon = TbOutlineWorld;
-export const KeyIcon = TbOutlineKey;
-export const HistoryIcon = TbOutlineHistory;
-export const PackageIcon = TbOutlinePackage;
-export const RobotIcon = TbOutlineRobot;
-export const BrainIcon = TbOutlineBrain;
-export const SparklesIcon = TbOutlineSparkles;
-export const FlameIcon = TbOutlineFlame;
-export const AntennaIcon = TbOutlineAntenna;
-export const LayersIntersectIcon = TbOutlineLayersIntersect;
-export const TbBranchIcon = TbOutlineGitBranch;
-export const TbFolderIcon = TbOutlineFolder;
-export const TbSearchIcon = TbOutlineSearch;
-export const TbTerminalIcon = TbOutlineTerminal2;
-export const TbRefreshIcon = TbOutlineRefresh;
-export const TbTrashIcon = TbOutlineTrash;
-export const AlertWarningIcon = TbOutlineAlertTriangle;
-export const EyeOpenIcon = TbOutlineEye;
-export const EyeClosedIcon = TbOutlineEyeOff;
-export const CopyIcon = TbOutlineCopy;
-export const CheckIcon = TbOutlineCheck;
+export const BoltIcon = IconBolt;
+export const RocketIcon = IconRocket;
+export const PlugIcon = IconPlug;
+export const GlobeIcon = IconWorld;
+export const KeyIcon = IconKey;
+export const HistoryIcon = IconHistory;
+export const PackageIcon = IconPackage;
+export const RobotIcon = IconRobot;
+export const BrainIcon = IconBrain;
+export const SparklesIcon = IconSparkles;
+export const FlameIcon = IconFlame;
+export const AntennaIcon = IconAntenna;
+export const LayersIntersectIcon = IconLayersIntersect;
+export const TbBranchIcon = IconGitBranch;
+export const TbFolderIcon = IconFolder;
+export const TbSearchIcon = IconSearch;
+export const TbTerminalIcon = IconTerminal2;
+export const TbRefreshIcon = IconRefresh;
+export const TbTrashIcon = IconTrash;
+export const AlertWarningIcon = IconAlertTriangle;
+export const EyeOpenIcon = IconEye;
+export const EyeClosedIcon = IconEyeOff;
+export const CopyIcon = IconCopy;
+export const CheckIcon = IconCheck;
